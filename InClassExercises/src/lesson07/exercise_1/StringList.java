06b8478a7f46e43ca70e764a9be9db2838ea2017
Hy Vo <94402902+hvo11152021@users.noreply.github.com>
2024-10-14 14:05:22 -0500

    update: Done default exercise

--- InClassExercises/src/lesson07/exercise_1/StringList.java
+++ InClassExercises/src/lesson07/exercise_1/StringList.java
@@ -1,15 +1,28 @@
 package lesson07.exercise_1;
 
 public interface StringList {
-		
+
 	String[] strArray();
+
 	int size();
+
 	void incrementSize();
-	
+
 	/** Adds a string to the end of the list */
-	public void add(String s);
-	
+	//	public void add(String s);
+
 	/** Gets the i_th string in the list */
-	public String get(int i);
-	
+	// public String get(int i);
+
+	default void add(String s) {
+		strArray()[size()] = s;
+		incrementSize();
+	}
+
+	default String get(int i) {
+		if (i < 0 || i >= size()) {
+			return null;
+		}
+		return strArray()[i];
+	}
 }
