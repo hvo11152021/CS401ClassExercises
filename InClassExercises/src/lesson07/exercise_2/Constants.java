3ba5ebcbf1c261bf25fd4c93a44f189eca20750e
Hy Vo <94402902+hvo11152021@users.noreply.github.com>
2024-10-14 14:11:59 -0500

    update: Done enum exercise

--- InClassExercises/src/lesson07/exercise_2/Constants.java
+++ InClassExercises/src/lesson07/exercise_2/Constants.java
@@ -1,6 +1,24 @@
 package lesson07.exercise_2;
 
-public class Constants {
-	public static final String COMPANY = "Microsoft";
-	public static final int SALES_TARGET = 20000000;
+public enum Constants {
+//	public static final String COMPANY = "Microsoft";
+//	public static final int SALES_TARGET = 20000000;
+	
+	BOTH("Microsoft", 20000000);
+	
+	private String s;
+	private int i;
+	
+	private Constants(String s, int i) {
+		this.s = s;
+		this.i = i;
+	}
+	
+	public String getName() {
+		return s;
+	}
+	
+	public int getSales() {
+		return i;
+	}
 }
--- InClassExercises/src/lesson07/exercise_2/Main.java
+++ InClassExercises/src/lesson07/exercise_2/Main.java
@@ -3,8 +3,11 @@ package lesson07.exercise_2;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(Constants.COMPANY);
-		System.out.println(Constants.SALES_TARGET);
+//		System.out.println(Constants.COMPANY);
+//		System.out.println(Constants.SALES_TARGET);
+
+		System.out.println(Constants.BOTH.getName());
+		System.out.println(Constants.BOTH.getSales());
 
 	}
 
