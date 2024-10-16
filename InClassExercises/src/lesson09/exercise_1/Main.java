0ac58674ca93832825491f1e4f11a4eb03a1be0f
Hy Vo <94402902+hvo11152021@users.noreply.github.com>
2024-10-16 11:36:19 -0500

    update: Fixed iterate exercise

--- InClassExercises/src/lesson09/exercise_1/Main.java
+++ InClassExercises/src/lesson09/exercise_1/Main.java
@@ -4,7 +4,7 @@ import java.util.stream.Stream;
 
 public class Main {
 	public static void main(String[] args) {
-		Stream<Integer> allOdds = Stream.iterate(1, n -> n + 2).limit(8);
+		Stream<Integer> allOdds = Stream.iterate(1, n -> n + 2).skip(4).limit(4);
 		allOdds.forEach(o -> System.out.println(o));
 	}
 }
