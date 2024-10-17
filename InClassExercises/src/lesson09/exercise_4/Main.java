438860568271c9ab078c95eee676c41190cbcffe
Hy Vo <94402902+hvo11152021@users.noreply.github.com>
2024-10-17 11:33:45 -0500

    update: Done concatenate exercise using reduce

--- InClassExercises/src/lesson09/exercise_4/Main.java
+++ InClassExercises/src/lesson09/exercise_4/Main.java
@@ -5,8 +5,8 @@ import java.util.stream.Stream;
 public class Main {
 	//Use the reduce method to produce a single space-separated String
 	public static void main(String[] args) {
-		Stream strings = Stream.of("A", "good", "day", "to", "write", "some", "Java");
-		
+		Stream<String> strings = Stream.of("A", "good", "day", "to", "write", "some", "Java");
+		System.out.println(strings.reduce("", (x , y) -> (x + y + " ")));
 	}
 
 }
