de1d7c78b2ed43eb218172f1443e2e0f21994f63
Hy Vo <94402902+hvo11152021@users.noreply.github.com>
2024-10-21 11:47:44 -0500

    update: Done wildcard capture exercise

--- InClassExercises/src/lesson11/exercise_4/Reverse.java
+++ InClassExercises/src/lesson11/exercise_4/Reverse.java
@@ -6,10 +6,14 @@ import java.util.*;
 public class Reverse {
 	//Fix this code using a helper method to capture the wildcard
 	public static void reverse(List<?> list) {   
-		List<Object> tmp = new ArrayList<Object>(list);   
+		reverseHelper(list);
+	}
+	
+	private static <T> void reverseHelper(List<T> list) {
+		List<T> tmp = new ArrayList<T>(list);   
 		for (int i = 0; i < list.size(); i++) { 
-	      //list.set(i, tmp.get(list.size() - i - 1)); //compiler error
-	   } 
+			list.set(i, tmp.get(list.size() - i - 1));
+		}
 	}
 	
 	//Test your code with this main method
